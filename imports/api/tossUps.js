d9942b0afa3f554c6002936d6e9dd47d73a12127
tf-venegas10 <tf.vengas10@uniandes.edu.co>
2018-03-25 23:22:57 -0500

    tossups collection

---
+++ imports/api/tossUps.js
@@ -0,0 +1,85 @@
+import {Meteor} from 'meteor/meteor';
+
+import {Mongo} from 'meteor/mongo';
+
+import {check} from 'meteor/check';
+
+export const TossUps = new Mongo.Collection('tossUps');
+
+if (Meteor.isServer) {
+
+    // This code only runs on the server
+
+    Meteor.publish('tossUps', function tasksPublication(userId) {
+
+         let all=TossUps.find();
+            all.filter((tossup)=>{
+                return (tossup.owners.indexOf(userId)!==-1);
+            });
+        return all;
+    });
+
+}
+
+Meteor.methods({
+
+    'tossUps.insert'(name) {
+
+        check(name, String);
+
+
+        // Make sure the user is logged in before inserting a tossUp
+
+        if (!this.userId) {
+
+            throw new Meteor.Error('not-authorized');
+
+        }
+
+
+        TossUps.insert({
+
+            name: name,
+            actions: [],
+            persons: [],
+            createdAt: new Date(),
+            owners: [this.userId],
+
+            usernames: [Meteor.users.findOne(this.userId).username],
+
+        });
+
+    },
+
+    'tossUps.remove'(tossUpId) {
+
+        check(tossUpId, String);
+
+        TossUps.remove(tossUpId);
+
+    },
+
+    'tossUps.addPerson'(tossUpId, personId, userName) {
+
+        check(tossUpId, String);
+        check(userName, String);
+
+        let thisToss= TossUps.findOne(tossUpId);
+        let persons= thisToss.persons;
+        persons.push({userName:userName, id: personId});
+        TossUps.update(tossUpId, {$set: {persons: persons}});
+
+    },
+    'tossUps.addAction'(tossUpId, action) {
+
+            check(tossUpId, String);
+            check(action, String);
+
+            let thisToss= TossUps.findOne(tossUpId);
+            let actions= thisToss.actions;
+            actions.push(action);
+            TossUps.update(tossUpId, {$set: {actions: actions}});
+
+        },
+
+});
