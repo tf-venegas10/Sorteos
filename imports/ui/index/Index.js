1c484dc6a8c0991aba565e93332c31f59d97754c
jmalovera10 <jma.lovera10@uniandes.edu.co>
2018-04-01 21:21:30 -0500

    Index explanation

--- imports/ui/index/Index.js
+++ imports/ui/index/Index.js
@@ -12,30 +12,41 @@ export default class Index extends Component {
                         <li data-target="#demo" data-slide-to="0" className="active"/>
                         <li data-target="#demo" data-slide-to="1"/>
                         <li data-target="#demo" data-slide-to="2"/>
+                        <li data-target="#demo" data-slide-to="3"/>
+                        <li data-target="#demo" data-slide-to="4"/>
                     </ul>
                     <div className="carousel-inner">
                         <div className="carousel-item active">
                             <h1 className="slogan">Make your own toss-ups with your friends</h1>
+                            <p className="content">Have fun challenging your friends or assign them tasks</p>
                         </div>
                         <div className="carousel-item">
-                            <div className="carousel-caption">
-                                <h3>Los Angeles</h3>
-                                <p>We had such a great time in LA!</p>
-                            </div>
+                            <h1 className="slogan">Person Mode</h1>
+                            <h3 className="subtitle">Want to select a person to do a job?</h3>
+                            <p className="content">This mode challenges a random person to do any task you already
+                                defined</p>
                         </div>
                         <div className="carousel-item">
-                            <div className="carousel-caption">
-                                <h3>Los Angeles</h3>
-                                <p>We had such a great time in LA!</p>
-                            </div>
+                            <h1 className="slogan">Action Mode</h1>
+                            <h3 className="subtitle">Actions is what you want?</h3>
+                            <p className="content">This mode challenges a person to do a random action</p>
+                        </div>
+                        <div className="carousel-item">
+                            <h1 className="slogan">Person and Action Mode</h1>
+                            <h3 className="subtitle">Want to have both options?</h3>
+                            <p className="content"> In this mode, you can randomly assign both an action and a person</p>
+                        </div>
+                        <div className="carousel-item">
+                            <h1 className="slogan">Toss4All Mode</h1>
+                            <h3 className="subtitle">Why not having fun in group?</h3>
+                            <p className="content">This mode is made to challenge all your friends to do customized
+                                actions</p>
                         </div>
                     </div>
                     <a className="carousel-control-prev" href="#demo" data-slide="prev">
-                        prev
                         <span className="carousel-control-prev-icon"/>
                     </a>
                     <a className="carousel-control-next" href="#demo" data-slide="next">
-                        next
                         <span className="carousel-control-next-icon"/>
                     </a>
                 </div>
