03978fcc2879b2b7c607e44b1e325caf156bbc9b
jmalovera10 <jma.lovera10@uniandes.edu.co>
2018-04-14 23:20:23 -0500

    Package install

--- imports/api/users.test.js
+++ imports/api/users.test.js
@@ -6,36 +6,34 @@ import {Users} from './users.js';
 
 // NOTE: Before writing a method like this you'll want to double check
 // that this file is only going to be loaded in test mode!!
-Meteor.methods({
-    'test.resetDatabase': () => resetDatabase(),
-});
+if(Meteor.isServer) {
+    describe('users', () => {
+        describe('methods', () => {
 
-describe('users', () => {
-    describe('methods', () => {
+            const userId = Random.id();
 
-        const userId = Random.id();
+            beforeEach(function () {
+                Users.remove({});
+            });
 
-        beforeEach(function () {
-            Users.remove({});
-        });
+            it('can insert user', () => {
+                const insertUser = Meteor.server._methodHandlers['appusers.insert'];
+                insertUser.apply(true);
+                assert.equal(Users.find().count, 0)
+            });
 
-        it('can insert user', () => {
-            const insertUser = Meteor.server._methodHandlers['appusers.insert'];
-            insertUser.apply(true);
-            assert.equal(Users.find().count, 0)
-        });
+            it('can find user', () => {
+                const findUser = Meteor.server._methodHandlers['appusers.find'];
+                let user = findUser.apply(userId);
+                assert.isNotNull(user);
+                assert.equals(users.userId, userId);
+            });
 
-        it('can find user', () => {
-            const findUser = Meteor.server._methodHandlers['appusers.find'];
-            let user = findUser.apply(userId);
-            assert.isNotNull(user);
-            assert.equals(users.userId,userId);
-        });
-
-        it('can set user offline', ()=>{
-            const setOffline = Meteor.server._methodHandlers['appusers.offline'];
-            let user = setOffline.apply();
-            assert.isTrue(user.online);
+            it('can set user offline', () => {
+                const setOffline = Meteor.server._methodHandlers['appusers.offline'];
+                let user = setOffline.apply();
+                assert.isTrue(user.online);
+            });
         });
     });
-});
+}
