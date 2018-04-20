309c0e174eaaa8be44dc9c49ea9428563b21690c
jmalovera10 <jma.lovera10@uniandes.edu.co>
2018-04-20 07:58:07 -0500

    User tests change

--- imports/api/users.test.js
+++ imports/api/users.test.js
@@ -6,32 +6,61 @@ import {Users} from './users.js';
 
 // NOTE: Before writing a method like this you'll want to double check
 // that this file is only going to be loaded in test mode!!
-if(Meteor.isServer) {
+if (Meteor.isServer) {
     describe('users', () => {
         describe('methods', () => {
 
-            const userId = Random.id();
-
             beforeEach(function () {
                 Users.remove({});
-                Users.insert({userId:1});
+                Meteor.users.remove({});
+                Accounts.createUser({
+                    email: "test@test.com",
+                    password: "testpa",
+                    username: "test"
+                }, (err)=>{
+                    if(!err){
+                        Users.insert({
+                            userId: 1,
+                            email: "test@test.com",
+                            username: "test",
+                            notifications: [],
+                            online: true
+                        });
+                    }
+                    else{
+                        console.log("Error on add")
+                    }
+                });
             });
 
             it('can insert user', () => {
-                Meteor.methods.call('appusers.insert', true);
-                assert.equal(Users.find().count(), 1)
+                Meteor.user().logout();
+                Accounts.createUser({
+                    email: "test1@test1.com",
+                    password: "testpa",
+                    username: "test1"
+                },(err)=>{
+                    Meteor.methods.call('appusers.insert', true);
+                });
+                assert.equal(Users.find().length(), 2)
             });
 
             it('can find user', () => {
-                let user = Meteor.methods.call('appusers.find',1);
-                let realUser=Users.find({userId:1});
-                assert.isNotNull(user);
-                assert.equals(user.userId, 1);
+                Meteor.methods.call('appusers.find', 1);
+                let realUser = Users.find({userId: 1}).fetch();
+                assert.isNotNull(realUser);
+                assert.equal(realUser.userId, 1);
             });
 
             it('can set user offline', () => {
-                const setOffline = Meteor.server._methodHandlers['appusers.offline'];
-                let user = setOffline.apply();
+                Meteor.call('appusers.offline');
+                let user = Users.find({userId:1});
+                assert.isFalse(user.online);
+            });
+
+            it('can set user online', () => {
+                Meteor.call('appusers.online');
+                let user = Users.find({userId:1});
                 assert.isTrue(user.online);
             });
         });
